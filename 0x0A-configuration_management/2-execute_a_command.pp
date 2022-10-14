1ce393a47cdbf7927a8fe1281324e6b9e4d47842
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2022-10-14 21:19:29 +0300

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,4 @@
-#  a manifest that kills a process named killmenow
-exec { 'pkill':
-  command => 'pkill -f killmenow',
-  path    => '/bin/bash',
-  return  => [0, 1],
+# Kill the process killmenow
+exec { 'pkill killmenow':
+  path    => '/usr/bin',
 }
