4e061cbee4c08800e506d83e0c805c5290cdd8d0
phiona-b3 <phiona030303@gmail.com>
2022-10-14 11:16:34 -0700

    Task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #  a manifest that kills a process named killmenow
 exec { 'pkill':
   command => 'pkill -f killmenow',
-  path    => '/usr/bin/',
+  path    => '/bin/bash',
   return  => [0, 1],
 }
