4831ecba066ccf8fda97ae49eb295b7c987c9d7b
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2022-11-16 21:43:34 +0300

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python3
-
-"""
-Python script that, using a REST API, for a given employee ID,
-returns information about his/her TODO list progress.
-"""
-
-from requests import get
-from sys import argv
-
-
+"""Solve an API"""
 if __name__ == "__main__":
-    response = get('https://jsonplaceholder.typicode.com/todos/')
-    data = response.json()
-    completed = 0
-    total = 0
-    tasks = []
-    response2 = get('https://jsonplaceholder.typicode.com/users')
-    data2 = response2.json()
+    import requests
+    import sys
 
-    for i in data2:
-        if i.get('id') == int(argv[1]):
-            employee = i.get('name')
+    user_id = int(sys.argv[1])
+    user_info = requests.get('https://jsonplaceholder.typicode.com/users?id={}'
+                             .format(user_id))
 
-    for i in data:
-        if i.get('userId') == int(argv[1]):
-            total += 1
+    try:
+        user = user_info.json()
+        if user_id is user[0].get("id"):
+            name_employ = user[0].get("name")
+    except:
+        print("Not a valid JSON")
 
-            if i.get('completed') is True:
-                completed += 1
-                tasks.append(i.get('title'))
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(user_id))
 
-    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
-                                                          total))
+    try:
+        tasks_json = tasks.json()
+        complete = 0
+        task_title = []
 
-    for i in tasks:
-        print("\t {}".format(i))
+        for task in tasks_json:
+            if task.get("completed") is True:
+                complete += 1
+                task_title.append(task.get("title"))
+    except:
+        print("Not a valid JSON")
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name_employ, complete, len(tasks_json)))
+    for title in task_title:
+        print("\t {}".format(title))
