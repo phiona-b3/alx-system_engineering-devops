5bf7f088b681423d051a726a0bd725a958c01c4d
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2022-12-01 19:40:56 +0300

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
-"""queries the reddit api"""
+"""Make a request"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers"""
-    try:
-        base_url = 'https://www.reddit.com/r/'
-        header = {'User-Agent': 'Mozilla/5.0'}
-        r = request.get(base_url +
-                        '{}/about.json'.format(subreddit), headers=header)
-        return r.json().get('data').get('subscribers')
-    except Exception:
+    """Make a request of a specific subreddit"""
+    url = 'https://api.reddit.com/r/{}/about.json'.format(subreddit)
+    header = {'User-agent': 'Mozilla/5.0'}
+    subred = requests.get(url, headers=header, allow_redirects=False)
+
+    if subred.status_code != 200:
         return 0
+
+    try:
+        theme = subred.json()
+    except:
+        print("Not a valid JSON")
+        return 0
+
+    data = theme.get("data")
+    if data:
+        subscribers = data.get("subscribers")
+        if subscribers:
+            return subscribers
+
+    return 0
