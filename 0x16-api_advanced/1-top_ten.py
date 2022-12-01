b82d193a929f8f2b787a1bbc9ec197cc9c20ef09
phiona-b3 <phiona030303@gmail.com>
2022-12-01 09:03:16 -0800

    Task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""queries the reddit api and prints the titles"""
+import requests
+
+
+def top_ten(subreddit):
+    """prints the titles of the first 10 hot posts"""
+    base_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    header = {'User-Agent': 'Mozilla/5.0'}
+    a = requests.get(base_url, headers=header, allow_redirects=False)
+
+    try:
+        requireddata = theme.get("data")
+        children = requireddata.get("children")
+        for child in children[:10]:
+            data = child.get("data")
+            print(data.get("title"))
+    except Exception:
+        return None
