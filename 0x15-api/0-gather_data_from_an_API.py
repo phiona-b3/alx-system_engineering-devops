740ded8cf355951464b8d820e33f59c14e681a4d
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2023-01-15 23:45:37 +0300

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,46 +1,21 @@
 #!/usr/bin/python3
-
-"""
-    Given employee ID, returns information about his/her TODO list progress.
-"""
-
+#  sends a request to the URL and displays the value of
+# the variable X-Request-Id in the response header
+""" script that fetches https://intranet.hbtn.io/status """
 
 import requests
 import sys
 
-base_url = 'https://jsonplaceholder.typicode.com/'
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/users/'
 
+    reqEmployee = requests.get(url + sys.argv[1])
+    reqTodos = requests.get(url + sys.argv[1] + '/todos/')
 
-def do_request():
-    '''Performs request'''
-    if len(sys.argv) < 2:
-        return print('USAGE:', __file__, '<employee id>')
-    eid = sys.argv[1]
-    try:
-        _eid = int(sys.argv[1])
-    except ValueError:
-        return print('Employee id must be an integer')
-
-    response = requests.get(base_url + 'users/' + eid)
-    if response.status_code == 404:
-        return print('User id not found')
-    elif response.status_code != 200:
-        return print('Error: status_code:', response.status_code)
-    user = response.json()
-
-    response = requests.get(base_url + 'todos/')
-    if response.status_code != 200:
-        return print('Error: status_code:', response.status_code)
-    todos = response.json()
-
-    user_todos = [todo for todo in todos
-                  if todo.get('userId') == user.get('id')]
-    completed = [todo for todo in user_todos if todo.get('completed')]
-    print('Employee', user.get('name'),
-          'is done with tasks({}/{}):'.
-          format(len(completed), len(user_todos)))
-    [print('\t', todo.get('title')) for todo in completed]
-
-
-if __name__ == '__main__':
-    do_request()
+    jsonemp = reqEmployee.json()
+    tasks = [key['completed'] for key in reqTodos.json()]
+    print("Employee {} is done with tasks({}/{}):".format(jsonemp.get('name'),
+          tasks.count(True), len(tasks)))
+    for task in reqTodos.json():
+        if task.get('completed') is True:
+            print("\t {}".format(task.get('title')))
